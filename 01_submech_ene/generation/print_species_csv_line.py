b30a6a6459420fa1931e8a94986bd90250e486a0
Andreas Copan <avcopan@gmail.com>
2024-08-05 14:39:51 -0400

    Re-generates hydrogen abstractions for ene submech

---
+++ 01_submech_ene/generation/print_species_csv_line.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+
+import automol
+
+args = sys.argv
+
+# smi = "C1=CCCC1"
+smi = args[1]
+chi = automol.smiles.amchi(smi)
+name = args[2] if len(args) > 2 else automol.amchi.chemkin_name(chi)
+smi = automol.amchi.smiles(chi)
+ich = automol.smiles.inchi(smi)
+ick = automol.inchi.inchi_key(ich)
+mult = automol.amchi.guess_spin(chi) + 1
+char = 0
+
+print(f"'{name}','{smi}','{ich}','{ick}',{mult},{char}")
