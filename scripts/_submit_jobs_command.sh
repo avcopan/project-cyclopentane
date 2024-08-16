a475ce633bb1fbbaeed79c70b608d5e1b073629e
avcopan <avcopan@gmail.com>
2024-08-16 12:17:58 -0500

    Wrapping up runs through Stage 3

--- scripts/_submit_jobs_command.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-NODES=${@}
-NODES_CSV="$(printf '%s,' ${NODES[@]})"
-
-echo "Running on $(hostname) in $(pwd)" && \
-echo "Process ID: $$" && \
-echo "Submitting with -n ${NODES_CSV}"  && \
-automech subtasks run-adhoc -n ${NODES_CSV} -a "$(pixi shell-hook)"
