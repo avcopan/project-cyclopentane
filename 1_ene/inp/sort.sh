5ca56d723bc99ca503c98a11adf12c69b5685dcc
Andreas Copan <avcopan@gmail.com>
2025-01-07 10:22:29 -0500

    Clean-up

--- 1_ene/inp/sort.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-WORK_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-TEMP_DIR=`mktemp -d -p "$WORK_DIR"`
-
-mechanalyzer sort -m orig_mechanism.dat -o mechanism.dat -c $TEMP_DIR/sort_species.csv
-
-rm -r $TEMP_DIR
