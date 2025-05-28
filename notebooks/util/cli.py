eb5945724f018a784c0a4984b386c2e42a045e36
Andreas Copan <avcopan@gmail.com>
2025-05-28 12:53:43 -0400

    New: Adds CLI for T-sweep simulation

--- notebooks/util/cli.py
+++ notebooks/util/cli.py
@@ -41,5 +41,27 @@ def simulate_o2(
     workflow.run_o2_simulation(tag=tag, root_path=root_path, gather_every=gather_every)
 
 
+@simulate.command("t")
+@click.argument("tag")
+@click.argument("root_path")
+@click.option("-e", "--gather_every", default=1, help="Run every n concentrations.")
+def simulate_t(
+    tag: str,
+    root_path: str | Path,
+    gather_every: int = 1,
+) -> None:
+    """Simulate model (JSR).
+
+    :param tag: Mechanism tag
+    :param root_path: Project root directory
+    :param temp_k: Temperature in K
+    :param pres_atm: Pressure in atm
+    :param tau_s: Residence time in s
+    :param vol_cm3: Volume in cm^3
+    :param gather_every: Gather every nth point
+    """
+    workflow.run_t_simulation(tag=tag, root_path=root_path, gather_every=gather_every)
+
+
 if __name__ == "__main__":
     main()
