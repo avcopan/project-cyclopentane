04ae215861ba94f621036d1382f9903872e1455f
avcopan <avcopan@gmail.com>
2025-09-01 11:23:48 -0400

    More progress on inserting OH addition TS...

--- insert/1e_A_rh-oh_C5H8-522_OH-4__C5H9O-852-r0/opt-dft2-alt/job.sh
+++ insert/1e_A_rh-oh_C5H8-522_OH-4__C5H9O-852-r0/opt-dft2-alt/job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=batch
 #SBATCH --job-name=test
-#SBATCH --ntasks=6
-#SBATCH --ntasks-per-node=6
+#SBATCH --ntasks=8
+#SBATCH --ntasks-per-node=8
 #SBATCH --cpus-per-task=1
 #SBATCH --time=72:00:00
-#SBATCH --mem-per-cpu=40G
+#SBATCH --mem-per-cpu=20G
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=avcopan@uga.edu
 
