7a3348c3d0bee86f281661b20fd0de8c1d84f484
avcopan <avcopan@gmail.com>
2025-09-24 10:16:56 -0400

    Re-organizes calculation folders

--- calc/Y_reactions_v0/fill/74/run.sh
+++
@@ -1,8 +0,0 @@
-TASK_ENV=$(cat << EOF
-$(pixi shell-hook -e dev2 --manifest-path /home/avcopan/proj/project-cyclopentane/code/mechdriver)
-module load gaussian/16-AVX2
-module load ORCA/5.0.4-gompi-2022a
-EOF
-)
-
-automech subtasks run -l "16 hr" -f "--partition=batch" -e "$TASK_ENV"
