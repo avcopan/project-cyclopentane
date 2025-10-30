5ad9f45d8f34db15924a4551ba9772efd611eec3
avcopan <avcopan@gmail.com>
2025-10-30 16:55:45 -0400

    Prepares new MESS calculations with H-scissions

--- calc/mess/C5H9/submit_mess.sh
+++ calc/mess/C5H9/submit_mess.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=mess
 #SBATCH --partition=batch
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=80GB
+#SBATCH --ntasks=8
+#SBATCH --mem-per-cpu=10GB
 #SBATCH --time=6:00:00
 #SBATCH --output=out.log
 #SBATCH --error=out.err
