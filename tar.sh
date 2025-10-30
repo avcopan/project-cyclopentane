e232447587f8428766967c3a12479a5257009433
avcopan <avcopan@gmail.com>
2025-10-30 16:34:16 -0400

    Extract MESS data after H-scissions added

--- tar.sh
+++ tar.sh
@@ -2,9 +2,8 @@
 #SBATCH --partition=batch
 #SBATCH --job-name=tar
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --time=4:00:00
+#SBATCH --time=6:00:00
 #SBATCH --mem=16G
 
-tar --use-compress-program="zstd -T0 -3" -cvf save.tar.zst save
+tar --zstd -cvf save.tar.zst save
 
